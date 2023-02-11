12027f7bdd284c19d191fa2454bb5aa4caa051ce
JESUS MARTINEZ HERRERO <jesus.martinez@tyris-software.com>
2023-02-11 15:17:42 +0100

    feat: create header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './componentes/Header';
 
 function App() {
   return (
     <div className="App">
-      Hola, React
+      <Header></Header>
     </div>
   );
 }
