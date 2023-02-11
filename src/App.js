c505c5990de7c993f6b10b33ec27004d83fa29c3
JESUS MARTINEZ HERRERO <jesus.martinez@tyris-software.com>
2023-02-11 16:04:18 +0100

    feat: useless new note form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './componentes/Footer';
 import Header from './componentes/Header';
+import NewNoteForm from './componentes/NewNoteForm';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NewNoteForm />
       <Footer />
     </div>
   );
