cc104f7091b4e5bd2a675bbf231dfb16b2832daf
JESUS MARTINEZ HERRERO <jesus.martinez@tyris-software.com>
2023-02-11 18:32:52 +0100

    feat: create note component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { Box } from '@mui/material';
 import './App.css';
 import Footer from './componentes/Footer';
 import Header from './componentes/Header';
 import NewNoteForm from './componentes/NewNoteForm';
+import Note from './componentes/Note';
 import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
@@ -28,6 +29,22 @@ function App() {
       <NewNoteForm 
         onAdd={addNote}
       />
+      <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap', mt: 2, mb: 6}}>
+        {
+          notes.map(
+            (note, index) => {
+              return ( 
+                <Note
+                  key={index}
+                  id={index}
+                  title={note.tile}
+                  content={note.content}
+                  />
+              ); 
+            }
+          )
+        }
+      </Box>
       <Footer />
     </div>
   );
